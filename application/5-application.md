ca678962bf74c003522786b9e324933bb328542d
cpage-pivotal <cpage@pivotal.io>
2021-12-31 07:39:04 -0600

    Streamlining Cartographer install

--- application/5-application.md
+++ application/5-application.md
@@ -5,7 +5,7 @@ In the application subdirectory, you will find a [workload.yaml](workload.yaml) 
 You have a few options on how to use your new environment:
 
 1. Leave the workload.yaml as is, and submit it for build and deploy.
-2. Fork the repo referenced in workload.yaml, and update the `spec.source.git.url` field in workload.yaml to point to your new repo. This will allow you to make subsequent updates to the code to use an iterative development process
+2. Fork the repo referenced in workload.yaml, and update the `spec.source.git.url` field in workload.yaml to point to your new forked repo. This will allow you to make subsequent updates to the code to use an iterative development process
 3. Update the `spec.source.git` configuration in workload.yaml to point to an application of your choosing. This application must be in one of the languages specified in the `kpack.languages` field of the config.yaml that you used to install Tanzu.
 
 Once your workload.yaml is ready, you can submit it:
--- install-cartographer.sh
+++ install-cartographer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -x
-SOURCE_CONTROLLER_VERSION=$(yq eval '.cartographer.source_controller_version' config.yaml)
 
 # Install SecretGen Controller
 kapp deploy --yes -a secretgen-controller -f setup/vendir/secretgen-controller
@@ -12,10 +11,7 @@ kapp deploy --yes -a cartographer -f setup/overlays/cartographer -f setup/vendir
 
 #  Install source controller,  the source part of the supply chain
 kubectl create namespace gitops-toolkit --dry-run=client -o yaml | kubectl apply -f -
-kapp deploy --yes -a gitops-toolkit \
-  --into-ns gitops-toolkit \
-  -f https://github.com/fluxcd/source-controller/releases/download/v$SOURCE_CONTROLLER_VERSION/source-controller.crds.yaml \
-  -f https://github.com/fluxcd/source-controller/releases/download/v$SOURCE_CONTROLLER_VERSION/source-controller.deployment.yaml
+kapp deploy --yes -a gitops-toolkit --into-ns gitops-toolkit -f setup/vendir/source-controller
 
 ## install sample supply chain 
 ytt --data-values-file config.yaml --ignore-unknown-comments -f source-to-knative-service  | kapp deploy -y -a source-to-knative-service -f-
