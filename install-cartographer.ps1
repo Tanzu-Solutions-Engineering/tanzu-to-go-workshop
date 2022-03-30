17ba1c276b90583a73819a0a2f8d7ccf5fa04161
Jeff <11721615+jeffellin@users.noreply.github.com>
2022-03-30 11:10:11 -0400

    use carvel package for fluxcd

--- install-cartographer.ps1
+++ install-cartographer.ps1
@@ -1,5 +1,6 @@
 $ENV:CARTOGRAPHER_PACKAGE_VERSION='0.2.2'
 $ENV:CERT_MANAGER_PACKAGE_VERSION='1.6.1'
+$ENV:FLUX_PACKAGE_VERSION=$(yq eval '.package.versions.flux' config.yaml)
 
 # Install SecretGen Controller
 
@@ -15,12 +16,10 @@ tanzu package install cert-manager \
 tanzu package install cartographer \
    --package-name cartographer.community.tanzu.vmware.com \
    --version $ENV:CARTOGRAPHER_PACKAGE_VERSION
-
-# Install RBAC required by cartographer.
-kapp deploy --yes -a cartographer-rbac -f setup/overlays/cartographer
 #  Install source controller,  the source part of the supply chain
-kubectl create namespace gitops-toolkit --dry-run=client -o yaml | kubectl apply -f -
-kapp deploy --yes -a gitops-toolkit --into-ns gitops-toolkit -f setup/vendir/source-controller
+tanzu package install fluxcd-source-controller \
+   --package-name fluxcd-source-controller.community.tanzu.vmware.com \
+   --version $ENV:FLUX_PACKAGE_VERSION
 
 ## install sample supply chain 
 ytt --data-values-file config.yaml --ignore-unknown-comments -f source-to-knative-service  | kapp deploy -y -a source-to-knative-service -f-
--- install-cartographer.sh
+++ install-cartographer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 export CARTOGRAPHER_PACKAGE_VERSION=$(yq eval '.package.versions.cartographer' config.yaml)
 export CERT_MANAGER_PACKAGE_VERSION=$(yq eval '.package.versions.cert-manager' config.yaml)
+export FLUX_PACKAGE_VERSION=$(yq eval '.package.versions.flux' config.yaml)
 
 # Install SecretGen Controller
 kapp deploy --yes -a secretgen-controller -f setup/vendir/secretgen-controller
@@ -16,11 +17,10 @@ tanzu package install cartographer \
    --package-name cartographer.community.tanzu.vmware.com \
    --version ${CARTOGRAPHER_PACKAGE_VERSION}
 
-# Install RBAC required by cartographer.
-kapp deploy --yes -a cartographer-rbac -f setup/overlays/cartographer
 #  Install source controller,  the source part of the supply chain
-kubectl create namespace gitops-toolkit --dry-run=client -o yaml | kubectl apply -f -
-kapp deploy --yes -a gitops-toolkit --into-ns gitops-toolkit -f setup/vendir/source-controller
+tanzu package install fluxcd-source-controller \
+   --package-name fluxcd-source-controller.community.tanzu.vmware.com \
+   --version ${FLUX_PACKAGE_VERSION}
 
 ## install sample supply chain 
 ytt --data-values-file config.yaml --ignore-unknown-comments -f source-to-knative-service  | kapp deploy -y -a source-to-knative-service -f-
